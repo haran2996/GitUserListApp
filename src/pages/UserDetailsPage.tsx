b74454f50626de9c6cf7cc0d74def5dfc44eda8e
haran2996 <haran2996@gmail.com>
2024-02-14 19:15:42 +0530

    adding back button

--- src/pages/UserDetailsPage.tsx
+++ src/pages/UserDetailsPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { UserDetails } from "../components/UserDetails/UserDetails"
 import { testData } from "../data"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { getUsersDetailsAction } from "../store/actions"
 import { useDispatch, useSelector } from "react-redux"
 import { APIState } from "../types"
@@ -11,6 +11,7 @@ import { Loading } from "../components/Loading/Loading"
 export const UserDetailsPage = () => {
     const currentUserDetails = useSelector((state: any) => state.user.currentUserDetails)
     const userDetailApi = useSelector((state: any) => state.user.userDetailsApiState)
+    const navigate = useNavigate();
     const userId = useParams();
     const dispatch = useDispatch();
     useEffect(() => {
@@ -20,10 +21,16 @@ export const UserDetailsPage = () => {
             })
         );
     }, [userId]);
-    if (userDetailApi === APIState.failed)
+    const renderComp = () => {
+        if (userDetailApi === APIState.failed)
         return <ErrorPage />
     else if (userDetailApi !== APIState.completed)
         return <Loading />
     else
         return <UserDetails userDetails={currentUserDetails} />
+    }
+    return <>
+        <button onClick={() => navigate('/')}>Back</button>
+        {renderComp()}
+    </>
 }
